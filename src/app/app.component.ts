8de3bcb513f796e152e8693c9609d10328a4a504
Nisha Shlaymoon <nishatoma@gmail.com>
2019-02-16 14:59:50 -0500

    Update
    Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,4 +16,16 @@ export class AppComponent {
   onBlueprintAdded(element: Element) {
     this.serverElements.push(element);
   }
+
+  onChangeFirst() {
+    if (this.serverElements.length > 0) {
+      this.serverElements[0].name = 'Changed!';
+    }
+  }
+
+  destroyElement() {
+    this.serverElements.pop();
+  }
+
+
 }
--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy, ViewChild, ElementRef, ContentChild } from '@angular/core';
 import { Element } from './element.model';
 
 @Component({
@@ -6,14 +6,70 @@ import { Element } from './element.model';
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css']
 })
-export class ServerElementComponent implements OnInit {
+// Make sure you always import the hook you wanna use
+// like OnInit, OnChanges, etc.
+export class ServerElementComponent implements OnInit,
+  OnChanges,
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked,
+  OnDestroy {
 
   // With the Input decorator, we can now use this element in another component
   @Input() element: Element;
+  @Input() name: string;
+  @ViewChild('heading') heading: ElementRef;
+  @ContentChild('contentParagraph') paragraph: ElementRef;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    console.log('Server Element Constructor called!');
   }
 
+  // This hook requires argument!
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('Server Element NgOnChanges called!');
+    console.log(changes);
+  }
+
+  ngOnInit() {
+    console.log('Server Element NgOnInit called!');
+    // Is heading lement available on init?
+    // This will be an empty space.
+    console.log('Heading content: ' + this.heading.nativeElement.textContent);
+    // What about content child paragraph?
+    console.log('Content Child: ' + this.paragraph.nativeElement.textContent);
+  }
+
+  // Runs everytime there is change detection run
+  ngDoCheck() {
+    console.log("Server Element ngDoCheck called!");
+  }
+
+  ngAfterContentInit() {
+    console.log('Server Element ngAfterContentInit called');
+    // Can cotent child show up after content init?
+    console.log('Content Child: ' + this.paragraph.nativeElement.textContent);
+  }
+
+  ngAfterContentChecked() {
+    console.log('Server Element ngAfterContentChecked called');
+  }
+
+  ngAfterViewInit() {
+    console.log('Server Element ngAfterViewInit called');
+    // What about after the view has initialized?
+    // This will print empty string still.
+    console.log(this.heading.nativeElement.textContent);
+  }
+
+  ngAfterViewChecked() {
+    console.log('Server Element ngAfterViewChecked called');
+  }
+
+  ngOnDestroy() {
+    console.log('Server Element ngOnDestroy called');
+  }
+  
 }
